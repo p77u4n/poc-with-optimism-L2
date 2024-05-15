a951d557461dc46ac1ad1506f88adc0cd48a0275
TuanCR <tuancr.pt@gmail.com>
2024-05-15 15:50:41 +0700

    deploy local success

--- packages/genomicdao/deploy/01_deploy_post_covid_stroke_prevention.ts
+++ packages/genomicdao/deploy/01_deploy_post_covid_stroke_prevention.ts
@@ -8,7 +8,7 @@ import { Contract } from "ethers";
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployBuyMeACoffee: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployPostCovidStrokePrevention: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
@@ -22,22 +22,23 @@ const deployBuyMeACoffee: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("BuyMeACoffee", {
+  await deploy("PostCovidStrokePrevention", {
     from: deployer,
     // Contract constructor arguments
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
     autoMine: true,
+    args: [1000000000, 15000, 3000, 225, 30],
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const buyMeACoffeeContract = await hre.ethers.getContract<Contract>("BuyMeACoffee", deployer);
-  console.log("👋 Buy this person a coffee!", await buyMeACoffeeContract.owner());
+  const contract = await hre.ethers.getContract<Contract>("PostCovidStrokePrevention", deployer);
+  console.log("👋 Token PCSP online!", await contract.owner());
 };
 
-export default deployBuyMeACoffee;
+export default deployPostCovidStrokePrevention;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags BuyMeACoffee
-deployBuyMeACoffee.tags = ["BuyMeACoffee"];
+deployPostCovidStrokePrevention.tags = ["PostCovidStrokePrevention"];
--- packages/genomicdao/deploy/02_deploy_controller.ts
+++ packages/genomicdao/deploy/02_deploy_controller.ts
@@ -1,18 +1,18 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { Contract } from "ethers";
+import { Controller, GeneNFT, PostCovidStrokePrevention } from "../typechain-types";
 
 /**
- * Deploys a contract named "YourContract" using the deployer account and
+ * Deploys a contract named "BuyMeACoffee" using the deployer account and
  * constructor arguments set to the deployer address
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployController: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
-    When deploying to live networks (e.g `yarn deploy --network sepolia`), the deployer account
+    When deploying to live networks (e.g `yarn deploy --network goerli`), the deployer account
     should have sufficient balance to pay for the gas fees for contract creation.
 
     You can generate a random account with `yarn generate` which will fill DEPLOYER_PRIVATE_KEY
@@ -21,24 +21,30 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   */
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
-
-  await deploy("YourContract", {
+  const nftContract = await hre.ethers.getContract<GeneNFT>("GeneNFT", deployer);
+  const pcspTokenContract = await hre.ethers.getContract<PostCovidStrokePrevention>(
+    "PostCovidStrokePrevention",
+    deployer,
+  );
+  await deploy("Controller", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
     autoMine: true,
+    args: [nftContract.target, pcspTokenContract.target],
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
+  const contract = await hre.ethers.getContract<Controller>("Controller", deployer);
+  await nftContract.transferOwnership(contract.target);
+  await pcspTokenContract.transferOwnership(contract.target);
+  console.log("👋 Controller online!", contract.target);
 };
 
-export default deployYourContract;
+export default deployController;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
-// e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+// e.g. yarn deploy --tags BuyMeACoffee
+deployController.tags = ["Controller"];
