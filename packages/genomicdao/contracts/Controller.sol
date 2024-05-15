02785b95054756ad0598dd78345d9e3a0ec42c72
TuanCR <tuancr.pt@gmail.com>
2024-05-15 11:49:25 +0700

    finish test

--- packages/genomicdao/contracts/Controller.sol
+++ packages/genomicdao/contracts/Controller.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.25;
 
 import "./NFT.sol" as nft;
 import "./Token.sol" as token;
+import "hardhat/console.sol";
 contract Controller {
 	//
 	// STATE VARIABLES
 	//
 	uint256 private _sessionIdCounter;
-	nft.GeneNFT public geneNFT;
-	token.PostCovidStrokePrevention public pcspToken;
+	nft.GeneNFT public geneNFTProxy;
+	token.PostCovidStrokePrevention public pcspTokenProxy;
+	uint256 callNum;
 
 	struct UploadSession {
 		uint256 id;
@@ -38,8 +40,8 @@ contract Controller {
 	event UploadData(string docId, uint256 sessionId);
 
 	constructor(address nftAddress, address pcspAddress) {
-		geneNFT = nft.GeneNFT(nftAddress);
-		pcspToken = token.PostCovidStrokePrevention(pcspAddress);
+		geneNFTProxy = nft.GeneNFT(nftAddress);
+		pcspTokenProxy = token.PostCovidStrokePrevention(pcspAddress);
 	}
 
 	function _initDoc(string memory docId) private {
@@ -51,8 +53,9 @@ contract Controller {
 		// TODO: Implement this method: to start an uploading gene data session.
 		// The doc id is used to identify a unique gene profile. Also should check
 		// if the doc id has been submited to the system before. This method return the session id
-		if (docSubmits[docId] == false) {
-			revert AlreadySubmitted(docId);
+		callNum += 1;
+		if (docSubmits[docId]) {
+			revert("Doc already been submitted");
 		} else {
 			_initDoc(docId);
 			uint256 newSessionId = _sessionIdCounter;
@@ -64,6 +67,7 @@ contract Controller {
 				docId
 			);
 			_sessionIdCounter += 1;
+			emit UploadData(docId, newSessionId);
 			return newSessionId;
 		}
 	}
@@ -88,30 +92,30 @@ contract Controller {
 		//certicate for his/her gene profile.
 		// TODO: Verify proof, we can skip this step
 		if (bytes(sessions[sessionId].docId).length == 0) {
-			revert("session is not exist");
+			revert("Session is not exist");
 		}
 		if (sessions[sessionId].user != msg.sender) {
-			revert("unauthorized");
+			revert("Invalid session owner");
 		}
 		if (sessions[sessionId].confirmed) {
-			revert("session already closed");
+			revert("Session is ended");
 		}
 		bytes memory refDocIdBytes = bytes(sessions[sessionId].docId);
 		bytes memory passingDocIdBytes = bytes(docId);
 		if (
-			refDocIdBytes.length == passingDocIdBytes.length &&
-			keccak256(refDocIdBytes) == keccak256(passingDocIdBytes)
+			!(refDocIdBytes.length == passingDocIdBytes.length &&
+				keccak256(refDocIdBytes) == keccak256(passingDocIdBytes))
 		) {
-			revert("docId and sessionId not belong to each other");
+			revert("DocId and SessionId not belong to each other");
 		}
 		if (verifyProof(proof) == false) {
 			revert ProofInvalid(proof);
 		} else {
 			docs[docId].hashContent = contentHash;
 			sessions[sessionId].proof = proof;
-			uint256 newNFTId = geneNFT.safeMint(msg.sender);
+			uint256 newNFTId = geneNFTProxy.safeMint(msg.sender);
 			nftDocs[newNFTId] = docId;
-			pcspToken.reward(msg.sender, riskScore);
+			pcspTokenProxy.reward(msg.sender, riskScore);
 			sessions[sessionId].confirmed = true;
 		}
 		// TODO: Update doc content
--- packages/genomicdao/test/Storage.js
+++ packages/genomicdao/test/Storage.js
@@ -1,164 +1,164 @@
-// const { time, loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { time, loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
-// const { expect } = require("chai");
+const { expect } = require("chai");
 
-// describe("Controller", function () {
-//   async function deployControllerFixture() {
-//     const [owner, addr1, addr2] = await ethers.getSigners();
+describe("Controller", function () {
+  async function deployControllerFixture() {
+    const [owner, addr1, addr2] = await ethers.getSigners();
 
-//     const nft = await ethers.deployContract("GeneNFT");
-//     const pcspToken = await ethers.deployContract("PostCovidStrokePrevention");
+    const nft = await ethers.deployContract("GeneNFT");
+    const pcspToken = await ethers.deployContract("PostCovidStrokePrevention", [1000000000, 15000, 3000, 225, 30]);
 
-//     const controller = await ethers.deployContract("Controller", [nft.target, pcspToken.target]);
+    const controller = await ethers.deployContract("Controller", [nft.target, pcspToken.target]);
 
-//     await nft.transferOwnership(controller.target);
-//     await pcspToken.transferOwnership(controller.target);
+    await nft.transferOwnership(controller.target);
+    await pcspToken.transferOwnership(controller.target);
 
-//     return { controller, nft, pcspToken, owner, addr1, addr2 };
-//   }
+    return { controller, nft, pcspToken, owner, addr1, addr2 };
+  }
 
-//   describe("Upload Data", function () {
-//     it("Should receive session id", async function () {
-//       const { controller } = await loadFixture(deployControllerFixture);
+  describe("Upload Data", function () {
+    it("Should receive session id", async function () {
+      const { controller } = await loadFixture(deployControllerFixture);
 
-//       await expect(controller.uploadData("doc1")).to.emit(controller, "UploadData").withArgs("doc1", 0);
-//     });
+      await expect(controller.uploadData("doc1")).to.emit(controller, "UploadData").withArgs("doc1", 0);
+    });
 
-//     it("Should fail if the doc is submited", async function () {
-//       const { controller, addr1, addr2 } = await loadFixture(deployControllerFixture);
+    it("Should fail if the doc is submited", async function () {
+      const { controller, addr1, addr2 } = await loadFixture(deployControllerFixture);
 
-//       const docId = "doc1";
-//       const contentHash = "dochash";
-//       const proof = "success";
-//       const riskScore = 1;
-//       const sessionId = 0;
+      const docId = "doc1";
+      const contentHash = "dochash";
+      const proof = "success";
+      const riskScore = 1;
+      const sessionId = 0;
 
-//       await controller.connect(addr1).uploadData(docId);
-//       await controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore);
+      await expect(controller.connect(addr1).uploadData(docId)).to.emit(controller, "UploadData").withArgs("doc1", 0);
+      await controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore);
 
-//       await expect(controller.connect(addr2).uploadData(docId)).to.be.revertedWith("Doc already been submitted");
-//     });
-//   });
+      await expect(controller.connect(addr1).uploadData(docId)).to.be.revertedWith("Doc already been submitted");
+    });
+  });
 
-//   describe("Confirm data", function () {
-//     it("Should receive correct nft", async function () {
-//       const { controller, nft, owner } = await loadFixture(deployControllerFixture);
+  describe("Confirm data", function () {
+    it("Should receive correct nft", async function () {
+      const { controller, nft, owner } = await loadFixture(deployControllerFixture);
 
-//       const docId = "doc1";
-//       const contentHash = "dochash";
-//       const proof = "success";
-//       const riskScore = 1;
-//       const sessionId = 0;
+      const docId = "doc1";
+      const contentHash = "dochash";
+      const proof = "success";
+      const riskScore = 1;
+      const sessionId = 0;
 
-//       await controller.uploadData(docId);
-//       await controller.confirm(docId, contentHash, proof, sessionId, riskScore);
+      await controller.uploadData(docId);
+      await controller.confirm(docId, contentHash, proof, sessionId, riskScore);
 
-//       expect(await nft.ownerOf(0)).to.equal(owner.address);
-//     });
+      expect(await nft.ownerOf(0)).to.equal(owner.address);
+    });
 
-//     it("Should receive correct pcsp reward", async function () {
-//       const { controller, pcspToken, addr1 } = await loadFixture(deployControllerFixture);
+    it("Should receive correct pcsp reward", async function () {
+      const { controller, pcspToken, addr1 } = await loadFixture(deployControllerFixture);
 
-//       const docId = "doc1";
-//       const contentHash = "dochash";
-//       const proof = "success";
-//       const riskScore = 1;
-//       const sessionId = 0;
+      const docId = "doc1";
+      const contentHash = "dochash";
+      const proof = "success";
+      const riskScore = 1;
+      const sessionId = 0;
 
-//       const awardAmount = BigInt("15000") * BigInt("10") ** BigInt("18");
+      const awardAmount = BigInt("15000") * BigInt("10") ** BigInt("18");
 
-//       await controller.connect(addr1).uploadData(docId);
-//       await controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore);
+      await controller.connect(addr1).uploadData(docId);
+      await controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore);
 
-//       const ownerBalance = await pcspToken.balanceOf(addr1.address);
+      const ownerBalance = await pcspToken.balanceOf(addr1.address);
 
-//       expect(ownerBalance).to.equal(awardAmount);
-//     });
+      expect(ownerBalance).to.equal(awardAmount);
+    });
 
-//     it("Should close session", async function () {
-//       const { controller, addr1 } = await loadFixture(deployControllerFixture);
+    it("Should close session", async function () {
+      const { controller, addr1 } = await loadFixture(deployControllerFixture);
 
-//       const docId = "doc1";
-//       const contentHash = "dochash";
-//       const proof = "success";
-//       const riskScore = 1;
-//       const sessionId = 0;
+      const docId = "doc1";
+      const contentHash = "dochash";
+      const proof = "success";
+      const riskScore = 1;
+      const sessionId = 0;
 
-//       await controller.connect(addr1).uploadData(docId);
-//       await controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore);
+      await controller.connect(addr1).uploadData(docId);
+      await controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore);
 
-//       const session = await controller.getSession(sessionId);
+      const session = await controller.getSession(sessionId);
 
-//       expect(session.proof).to.equal(proof);
-//       expect(session.confirmed).to.equal(true);
-//     });
+      expect(session.proof).to.equal(proof);
+      expect(session.confirmed).to.equal(true);
+    });
 
-//     it("Should content hash uploaded", async function () {
-//       const { controller, addr1 } = await loadFixture(deployControllerFixture);
+    it("Should content hash uploaded", async function () {
+      const { controller, addr1 } = await loadFixture(deployControllerFixture);
 
-//       const docId = "doc1";
-//       const contentHash = "dochash";
-//       const proof = "success";
-//       const riskScore = 1;
-//       const sessionId = 0;
+      const docId = "doc1";
+      const contentHash = "dochash";
+      const proof = "success";
+      const riskScore = 1;
+      const sessionId = 0;
 
-//       await controller.connect(addr1).uploadData(docId);
-//       await controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore);
+      await controller.connect(addr1).uploadData(docId);
+      await controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore);
 
-//       const doc = await controller.getDoc(docId);
+      const doc = await controller.getDoc(docId);
 
-//       expect(doc.hashContent).to.equal(contentHash);
-//     });
+      expect(doc.hashContent).to.equal(contentHash);
+    });
 
-//     it("Should fail if the doc is submitted", async function () {
-//       const { controller, addr1 } = await loadFixture(deployControllerFixture);
+    it("Should fail if the doc is submitted", async function () {
+      const { controller, addr1 } = await loadFixture(deployControllerFixture);
 
-//       const docId = "doc1";
-//       const contentHash = "dochash";
-//       const proof = "success";
-//       const riskScore = 1;
-//       const sessionId = 0;
+      const docId = "doc1";
+      const contentHash = "dochash";
+      const proof = "success";
+      const riskScore = 1;
+      const sessionId = 0;
 
-//       await controller.connect(addr1).uploadData(docId);
-//       await controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore);
+      await controller.connect(addr1).uploadData(docId);
+      await controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore);
 
-//       await expect(
-//         controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore),
-//       ).to.be.revertedWith("Doc already been submitted");
-//     });
+      await expect(
+        controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore),
+      ).to.be.revertedWith("Session is ended");
+    });
 
-//     it("Should fail if the session owner is invalid", async function () {
-//       const { controller, addr1, addr2 } = await loadFixture(deployControllerFixture);
+    it("Should fail if the session owner is invalid", async function () {
+      const { controller, addr1, addr2 } = await loadFixture(deployControllerFixture);
 
-//       const docId = "doc1";
-//       const contentHash = "dochash";
-//       const proof = "success";
-//       const riskScore = 1;
-//       const sessionId = 0;
+      const docId = "doc1";
+      const contentHash = "dochash";
+      const proof = "success";
+      const riskScore = 1;
+      const sessionId = 0;
 
-//       await controller.connect(addr1).uploadData(docId);
+      await controller.connect(addr1).uploadData(docId);
 
-//       await expect(
-//         controller.connect(addr2).confirm(docId, contentHash, proof, sessionId, riskScore),
-//       ).to.be.revertedWith("Invalid session owner");
-//     });
+      await expect(
+        controller.connect(addr2).confirm(docId, contentHash, proof, sessionId, riskScore),
+      ).to.be.revertedWith("Invalid session owner");
+    });
 
-//     it("Should fail if the session is end", async function () {
-//       const { controller, addr1 } = await loadFixture(deployControllerFixture);
+    it("Should fail if the session and document is not correspondent", async function () {
+      const { controller, addr1 } = await loadFixture(deployControllerFixture);
 
-//       const docId = "doc1";
-//       const docId2 = "doc2";
-//       const contentHash = "dochash";
-//       const proof = "success";
-//       const riskScore = 1;
-//       const sessionId = 0;
+      const docId = "doc1";
+      const docId2 = "doc2";
+      const contentHash = "dochash";
+      const proof = "success";
+      const riskScore = 1;
+      const sessionId = 0;
 
-//       await controller.connect(addr1).uploadData(docId);
-//       await controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore);
+      await controller.connect(addr1).uploadData(docId);
+      // await controller.connect(addr1).confirm(docId, contentHash, proof, sessionId, riskScore);
 
-//       await expect(
-//         controller.connect(addr1).confirm(docId2, contentHash, proof, sessionId, riskScore),
-//       ).to.be.revertedWith("Session is ended");
-//     });
-//   });
-// });
+      await expect(
+        controller.connect(addr1).confirm(docId2, contentHash, proof, sessionId, riskScore),
+      ).to.be.revertedWith("DocId and SessionId not belong to each other");
+    });
+  });
+});
